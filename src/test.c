e2f519f03918f4fdd616346c12fcd66a2cf495a8
Lucass307 <lucas.defaud@gmail.com>
2024-01-10 22:37:21 +0100

    pitié

--- src/test.c
+++ src/test.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "vigenere_file.h"
 
 // Inclure le code que vous souhaitez tester
+#include "vigenere_file.h"
+
 void setUp(void) {
     // Initialiser avant chaque test si nécessaire
 }
@@ -13,15 +14,23 @@ void tearDown(void) {
     // Nettoyer après chaque test si nécessaire
 }
 
+void run_program(char *input_file, char *key, char *output_file, int is_decode) {
+    char *argv[5];
+    argv[0] = "program_name";
+    argv[1] = input_file;
+    argv[2] = key;
+    argv[3] = is_decode ? "decode" : "";
+    argv[4] = NULL;
+
+    // Appeler votre fonction vigenere_main avec les arguments
+    vigenere_main(is_decode ? 4 : 3, argv);
+}
+
 void test_vigenere_encrypt() {
-    // Test case 1
-    FILE *input1 = fopen("test_input1.txt", "w");
-    fputs("HELLO", input1);
-    fclose(input1);
+    // Exécuter votre programme pour chiffrer
+    run_program("test_input1.txt", "KEY", "test_output1.txt", 0);
 
-    char *argv1[] = {"program_name", "test_input1.txt", "KEY"};
-    vigenere_main(3, argv1);
-
+    // Lire le fichier résultant et vérifier le résultat
     FILE *result1 = fopen("test_output1.txt", "r");
     char buffer1[1024];
     fgets(buffer1, sizeof(buffer1), result1);
@@ -31,14 +40,10 @@ void test_vigenere_encrypt() {
 }
 
 void test_vigenere_decrypt() {
-    // Test case 1
-    FILE *input1 = fopen("test_input1.txt", "w");
-    fputs("RIJVS", input1);
-    fclose(input1);
+    // Exécuter votre programme pour déchiffrer
+    run_program("test_input1.txt", "KEY", "test_output1.txt", 1);
 
-    char *argv1[] = {"program_name", "test_input1.txt", "KEY", "decode"};
-    vigenere_main(4, argv1);
-
+    // Lire le fichier résultant et vérifier le résultat
     FILE *result1 = fopen("test_output1.txt", "r");
     char buffer1[1024];
     fgets(buffer1, sizeof(buffer1), result1);
